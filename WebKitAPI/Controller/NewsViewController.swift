da30a049a9cf8007b29ff2669c425549bd4f020e
KodAndN <andrkokaz@gmail.com>
2023-02-18 14:31:53 +0300

    API<

--- WebKitAPI/Controller/NewsViewController.swift
+++ WebKitAPI/Controller/NewsViewController.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 final class NewsViewController: UIViewController {
-    
-    private lazy var tableView: UITableView = {
+
+    private lazy var tableView : UITableView = {
         let tableView = UITableView()
         tableView.separatorStyle = .none
         tableView.refreshControl = refreshControl
@@ -18,10 +18,10 @@ final class NewsViewController: UIViewController {
         tableView.dataSource = self
         tableView.delegate = self
         tableView.register(NewsTableViewCell.self, forCellReuseIdentifier: NewsTableViewCell.identifier)
-        
+
         return tableView
     }()
-
+    
     private lazy var refreshControl: UIRefreshControl = {
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(refreshData), for: .valueChanged)
@@ -35,10 +35,9 @@ final class NewsViewController: UIViewController {
         super.viewDidLoad()
         title = "News"
         view.backgroundColor = .systemBackground
-//        loadData()
+        loadData()
         setup()
     }
-    
     
     private func setup() {
         view.addSubview(tableView)
@@ -51,10 +50,50 @@ final class NewsViewController: UIViewController {
         ])
     }
     
+    private func loadData() {
+        NetworkService.shared.getContent { [weak self] result in
+            switch result {
+            case .success(let articles):
+                let shuffled = articles.shuffled()
+                let pagination20 = Array(shuffled.prefix(20))
+                self?.model = pagination20.compactMap { article in
+                    NewsModel(
+                        title: article.title,
+                        publisher: article.source.name,
+                        description: article.description ?? "No description has been provided",
+                        websiteUrl: article.websiteUrl ?? "",
+                        date: self?.formatDate(dateString: article.date) ?? "Date is unknown",
+                        imageUrl: URL(string: article.imageUrl ?? ""))
+                }
+                
+                if let len = self?.model.count {
+                    self?.views = [Views](repeating: Views(counter: 0), count: len)
+                }
+                
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    private func formatDate(dateString: String?) -> String {
+        guard let dateString = dateString else { return "Date is unknown" }
+        if let date = ISO8601DateFormatter().date(from: dateString) {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "MMMM d, yyyy"
+            let result = dateFormatter.string(from: date)
+            return result
+        }
+        return "Date is unknown"
+    }
     
     @objc private func refreshData() {
         print("Refreshed")
-
+        loadData()
         refreshControl.endRefreshing()
     }
 }
@@ -76,8 +115,14 @@ extension NewsViewController: UITableViewDelegate, UITableViewDataSource {
         cell.count.text = "View count: \(views[indexPath.row].counter)"
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        views[indexPath.row].counter += 1
+        tableView.reloadData()
+        let nextController = DetailViewController(model: model[indexPath.row])
+        navigationController?.pushViewController(nextController, animated: true)
+    }
 }
-
 
 struct Views {
     var counter: Int = 0
